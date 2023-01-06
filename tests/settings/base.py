a21331a8c0cca43906825ca367028e59f9005fa6
Andrew-Chen-Wang <acwangpython@gmail.com>
2023-01-05 20:51:57 -0500

    Add Django Redis core client
    Signed-off-by: Andrew-Chen-Wang <acwangpython@gmail.com>

--- tests/settings/base.py
+++ tests/settings/base.py
@@ -1,5 +1,12 @@
 SECRET_KEY = "django_tests_secret_key"
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
 CACHES = {
     "default": {
         "BACKEND": "django_async_redis.cache.RedisCache",
@@ -22,6 +29,10 @@ CACHES = {
         "OPTIONS": {"CLIENT_CLASS": "django_async_redis.client.DefaultClient"},
         "KEY_PREFIX": "test-prefix",
     },
+    "core": {
+        "BACKEND": "django_async_redis.client.core.AsyncRedisCache",
+        "LOCATION": "redis://127.0.0.1:6379?db=1",
+    },
 }
 
-INSTALLED_APPS = ["django.contrib.sessions"]
+INSTALLED_APPS = ["django.contrib.sessions", "tests.app"]
